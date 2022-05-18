9067fde1222e0bb7fcff9b8a2472541bd2ef1b26
Walid Wazini <wazini25@gmail.com>
2022-05-18 11:09:55 +0800

    fix form and update ui

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,35 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 
-const SearchBar = ({ fetch }) => {
-  const [search, setSearch] = useState("");
-
-  const searchHandler = async (e) => {
-    if (e.key === "Enter") {
-      console.log("Entered");
-      fetch(e.target.value);
-      setSearch("");
-    }
-  };
-
-  const changeHandler = (ev) => {
-    console.log(ev.target.value);
-  };
-
+const SearchBar = ({
+  type,
+  buttonType,
+  id,
+  value,
+  placeholder,
+  onChange,
+  onBlur,
+  onClick,
+  disabled,
+}) => {
   return (
     <div className='h-1/4'>
       <div className='relative w-[400px]'>
         <div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'></div>
         <input
-          type='text'
-          id='city-search'
-          placeholder='Search a city..'
-          onKeyPress={searchHandler}
+          placeholder={placeholder}
+          type={type}
+          id={id}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
           className='block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
           required
         />
         <button
-          type='submit'
-          class='text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+          onClick={onClick}
+          type={buttonType || "button"}
+          disabled={disabled}
+          className='text-white absolute right-2.5 bottom-2.5 bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
         >
           Search
         </button>
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import SearchBar from "../components/SearchBar";
 
-const testWeather = (e) => {
-  console.log(e);
-};
-
 const Main = () => {
+  const searchRef = useRef();
+  const [search, setSearch] = useState("");
+  const [cityName, setCityName] = useState("City Name");
+
+  const changeHandler = (ev) => setSearch(ev.target.value);
+
+  const submitHandler = (ev) => {
+    ev.preventDefault();
+    const searchCity = search;
+    console.log(searchCity);
+    setCityName(searchCity);
+    setSearch("");
+  };
+
   return (
-    <div className='basis-3/4 bg-red-400 w-1/2 h-full'>
+    <div className='basis-3/4 w-1/2 h-full'>
       <div className='h-full w-full flex flex-row items-center justify-center'>
         <div className='flex flex-col h-3/4'>
-          <SearchBar fetch={testWeather} />
+          <form onSubmit={submitHandler}>
+            <SearchBar
+              // ref={searchRef}
+              value={search}
+              onChange={changeHandler}
+              placeholder='Search a city..'
+              color={"bg-red-70"}
+              buttonType='submit'
+            />
+          </form>
           <div className='h-3/4'>
-            <h1>City Display</h1>
+            <div
+              id='city-display'
+              className='h-4/5 flex flex-col justify-center items-center bg-yellow-50 mt-5 px-4 py-[8%] shadow-md '
+            >
+              <div id='city-name'>
+                <h2 className='text-[30px] text-red-500'>{cityName}</h2>
+              </div>
+            </div>
           </div>
         </div>
       </div>
